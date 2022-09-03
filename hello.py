164ccdab107a3caec476fd2da426cd6335090470
Daniel Santos <daniel.santos@dss.tec.br>
2022-09-03 15:31:06 -0300

    Feature Many Languages added

--- hello.py
+++ hello.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python3
+""" Hello World Many Languages.
 
-# This program printing the Hello World message
+This program printing the Hello World message based on the LANG enviroment variable of system.
 
-print('Hello, World!')
+How to use:
+    You need have the LANG variable configured on system. Case the LANG variable were unset,
+the message will be showed in english.
+
+Excute:
+    ./hello.py
+"""
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+
+print(msg)
